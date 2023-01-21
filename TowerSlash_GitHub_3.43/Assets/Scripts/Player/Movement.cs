8094b1fe55d2af741d7aea6710bc909215a1ee06
Miggy Bagadiong <miggy.0825@gmail.com>
2023-01-21 16:30:06 +0800

    added upward movement

---
+++ TowerSlash_GitHub_3.43/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float playerSpeed;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float up = playerSpeed * Time.deltaTime;
+        this.transform.position += new Vector3(0, up, 0);
+    }
+}
