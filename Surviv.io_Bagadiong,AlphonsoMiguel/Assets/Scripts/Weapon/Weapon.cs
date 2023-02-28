2b4310e3a9589818b433597a3369044ae8927b8f
Miggy Bagadiong <miggy.0825@gmail.com>
2023-02-28 16:20:00 +0800

    m1 progress
    added weapon scriptable object

---
+++ Surviv.io_Bagadiong,AlphonsoMiguel/Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType{
+    Pistol = 1,
+    Shotgun = 2,
+    Rifle = 3
+};
+
+[CreateAssetMenu(menuName = "Weapon")]
+public class Weapon : ScriptableObject
+{
+    [Header("Weapon Type")]
+    public WeaponType weaponType;
+
+    [Header("Weapon Ammo")]
+    public int wCurrAmmo;
+    public int wMaxAmmo;
+
+    [Header("Weapon Firing")]
+    public float wFireRate;
+    public bool hasSpread;
+    public bool isAutomatic;
+
+    [Header("Bullet")]
+    public GameObject wBullet;
+    public float wBulletSpeed = 20f;    
+
+}
